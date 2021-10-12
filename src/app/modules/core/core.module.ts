2185dd0213a431a421865929f9d04b0e8d56354f
fengelmann <github@fengelmann.de>
2021-10-12 12:35:56 +0200

    Add core components

---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CdsModule } from '@cds/angular';
+import { CoreComponent } from './components/core/core.component';
+import {CoreRoutingModule} from './core-routing.module';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LandingComponent } from './components/landing/landing.component';
+
+@NgModule({
+  declarations: [
+    CoreComponent,
+    NavigationComponent,
+    LandingComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreRoutingModule,
+    CdsModule,
+    ClarityModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [CoreComponent]
+})
+export class CoreModule { }
