f51f6028ad1ed93196fd539ca4f35ed8f0a236b4
fengelmann <github@fengelmann.de>
2021-10-29 10:03:41 +0200

    Add documentation

--- src/app/modules/shared/directives/if-error.directive.ts
+++ src/app/modules/shared/directives/if-error.directive.ts
@@ -1,40 +1,67 @@
-import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
+import {Directive, Input, OnDestroy, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
 import {NgControl} from '@angular/forms';
 import {FormControlService} from '../services/form-control.service';
+import {Subscription} from 'rxjs';
 
-// TODO unsubscribe on destroy
-
+/**
+ * Directive for rendering cds control messages depending on the error state of the control. If the specified error
+ * is present, the control message is rendered, if not is is hidden.
+ */
 @Directive({
   selector: '[bchIfError]'
 })
-export class IfErrorDirective implements OnInit {
+export class IfErrorDirective implements OnInit, OnDestroy {
 
-  @Input() bchIfErrorControlName: string;
+  @Input() bchIfErrorControlName: string; // Name of the Angular control on which to look for errors
   @Input() bchIfError: string; // Name of the error to react on
 
   private hasView = false;
   private control: NgControl;
   private touched = false;
+  private controlElementTouchedChangesSubscription: Subscription;
+  private statusChangesSubscription: Subscription;
 
   constructor(private templateRef: TemplateRef<any>,
               private formControlService: FormControlService,
               private viewContainer: ViewContainerRef) {
   }
 
+  /**
+   * First, the view is destroyed as the initial state of the form is untouched. Then the control is fetched by using
+   * the set control name and status and touched events are subscribed to to react on changes by showing or hiding
+   * the error messages.
+   */
   ngOnInit() {
-    this.control = this.formControlService.getControl(this.bchIfErrorControlName);
     this.destroyView();
-    this.formControlService.controlElementTouchedChanges(this.bchIfErrorControlName).subscribe(touched => {
-      this.touched = touched;
-      this.checkStatus();
-    });
-
-
-    this.control.statusChanges?.subscribe(() => {
-      this.checkStatus();
-    });
+    this.control = this.formControlService.getControl(this.bchIfErrorControlName);
+    // When the control becomes touched, we may need to show the error messages
+    this.controlElementTouchedChangesSubscription =
+      this.formControlService.controlElementTouchedChanges(this.bchIfErrorControlName).subscribe(touched => {
+        this.touched = touched;
+        this.checkStatus();
+      });
+    // When the controls status (valid, invalid etc) changes, we may need to show the error messages
+    if (this.control.statusChanges) {
+      this.statusChangesSubscription = this.control.statusChanges.subscribe(() => {
+        this.checkStatus();
+      });
+    }
   }
 
+  /**
+   * Unsubscribes from Observables when the directives life ends.
+   */
+  ngOnDestroy() {
+    this.controlElementTouchedChangesSubscription?.unsubscribe();
+    this.statusChangesSubscription?.unsubscribe();
+  }
+
+  /**
+   * Checks the current state of the directive. If the control is invalid and has been touched the view is created
+   * (meaning the error message is shown). If not, it is destroyed. In any case the FormControlService is notified
+   * about probable error state changes. This is needed for the ifNoError directive to show or hide the other
+   * control messages accordingly.
+   */
   checkStatus() {
     if (this.control.invalid && this.touched) {
       this.createView();
@@ -44,6 +71,9 @@ export class IfErrorDirective implements OnInit {
     this.formControlService.onErrorStateChange();
   }
 
+  /**
+   * Creates the view if it is not already shown.
+   */
   createView() {
     if (!this.hasView) {
       this.viewContainer.createEmbeddedView(this.templateRef);
@@ -51,12 +81,14 @@ export class IfErrorDirective implements OnInit {
     }
   }
 
+  /**
+   * Clears the view container if the view was previously shown.
+   */
   destroyView() {
     if (this.hasView) {
       this.viewContainer.clear();
       this.hasView = false;
     }
   }
-
 
 }
